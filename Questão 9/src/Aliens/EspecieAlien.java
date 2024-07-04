4adf6669db9a89c089208d9696d8ab24ff4ee321
Matt <164085448+MattKings27@users.noreply.github.com>
2024-07-03 21:14:45 -0400

    Delete Questão 9/src directory

--- Questão 9/src/Aliens/EspecieAlien.java
+++
@@ -1,7 +0,0 @@
-package Aliens;
-
-public class EspecieAlien extends Especie {
-    public EspecieAlien(String nome, String planetaOrigem, int nivelPericulosidadeBase) {
-        super(nome, planetaOrigem, nivelPericulosidadeBase);
-    }
-}
